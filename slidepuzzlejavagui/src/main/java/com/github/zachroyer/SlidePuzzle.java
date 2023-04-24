8ebdfabc56ea9266f6d36fea27aeabdad52d9321
Zach Royer <zach.royer14@gmail.com>
2023-04-23 21:59:15 -0600

    .

--- slidepuzzlejavagui/src/main/java/com/github/zachroyer/SlidePuzzle.java
+++ slidepuzzlejavagui/src/main/java/com/github/zachroyer/SlidePuzzle.java
@@ -4,8 +4,6 @@ import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 import java.awt.Container;
 import java.awt.Font;
@@ -15,21 +13,23 @@ import java.util.ArrayList;
 
 public class SlidePuzzle implements ActionListener {
 
-    public SlidePuzzle() {
+    int[] grid;
+    JButton[] buttons;
 
+    public SlidePuzzle() {
         JFrame window = new JFrame("Slide Puzzle");
         window.setLayout(new GridLayout(3, 3, 5, 5));
         window.setSize(400, 400);
         window.setLocationRelativeTo(null);
 
-        JButton[] buttons = new JButton[9];
-        ArrayList<Integer> grid = new ArrayList(9);
+        buttons = new JButton[9];
+        grid = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, }; //Represents JButton location in grid
+        
         for (int i = 0; i < 9; i++) {
             if (i != 8) {
                 buttons[i] = new JButton(String.valueOf(i + 1));
                 window.add(buttons[i]);
                 buttons[i].setFont(new Font("Arial", Font.PLAIN, 40));
-                grid.add(i);
                 buttons[i].addActionListener(this);
             } else {
                 buttons[i] = new JButton();
@@ -57,6 +57,13 @@ public class SlidePuzzle implements ActionListener {
         Container temp = buttonOne.getParent();
         Container temp2 = buttonTwo.getParent();
         temp.setComponentZOrder(temp, buttonOne.);
+
+    }
+
+    private void swapValues(int index1, int index2) {
+        int temp = grid[index1];
+        grid[index1] = grid[index2];
+        grid[index2] = temp;
 
     }
 }
