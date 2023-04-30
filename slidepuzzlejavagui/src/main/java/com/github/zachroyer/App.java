700615d551a53aa3a362730849f1c3d4839cc1ad
Zach Royer <zach.royer14@gmail.com>
2023-04-30 12:33:13 -0600

    Added isValidSwap method Fixed swap method Switched over to ArrayList for methods

--- slidepuzzlejavagui/src/main/java/com/github/zachroyer/App.java
+++ slidepuzzlejavagui/src/main/java/com/github/zachroyer/App.java
@@ -1,5 +1,8 @@
 package com.github.zachroyer;
-
+/**
+*
+* @author Zachary Royer
+*/
 public class App {
     public static void main(String[] args) {
         SlidePuzzle puzzle = new SlidePuzzle();
--- slidepuzzlejavagui/src/main/java/com/github/zachroyer/SlidePuzzle.java
+++ slidepuzzlejavagui/src/main/java/com/github/zachroyer/SlidePuzzle.java
@@ -1,39 +1,45 @@
 package com.github.zachroyer;
 
+/**
+*
+* @author Zachary Royer
+*/
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Window;
 import java.util.ArrayList;
 
 public class SlidePuzzle implements ActionListener {
 
-    int[] grid;
-    JButton[] buttons;
+    private ArrayList<JButton> buttons;
+    private JFrame window;
+    private JButton emptyButton;
 
     public SlidePuzzle() {
-        JFrame window = new JFrame("Slide Puzzle");
+        window = new JFrame("Slide Puzzle");
         window.setLayout(new GridLayout(3, 3, 5, 5));
         window.setSize(400, 400);
         window.setLocationRelativeTo(null);
 
-        buttons = new JButton[9];
-        grid = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, }; //Represents JButton location in grid
-        
+        buttons = new ArrayList<>(9);
+
         for (int i = 0; i < 9; i++) {
             if (i != 8) {
-                buttons[i] = new JButton(String.valueOf(i + 1));
-                window.add(buttons[i]);
-                buttons[i].setFont(new Font("Arial", Font.PLAIN, 40));
-                buttons[i].addActionListener(this);
+                buttons.add(i, new JButton(String.valueOf(i + 1)));
+                window.add(buttons.get(i));
+                buttons.get(i).setFont(new Font("Arial", Font.PLAIN, 40));
+                buttons.get(i).addActionListener(this);
+
             } else {
-                buttons[i] = new JButton();
-                window.add(buttons[i]);
+                emptyButton = new JButton();
+                buttons.add(i, emptyButton);
+                window.add(emptyButton);
 
             }
         }
@@ -41,29 +47,34 @@ public class SlidePuzzle implements ActionListener {
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        System.out.println(grid.toString());
-        System.out.println(buttons.toString());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        (e.getSource());
-        swap(e.getSource(), )
-        // System.out.println("Button" + JButton.indexOf(this) + "clicked");
+        JButton buttonClicked = (JButton) (e.getSource()); // JButton object, type cast of the button source from ActionListener
+        int clickedIndex = buttons.indexOf(buttonClicked);  
+        int emptyIndex = buttons.indexOf(emptyButton);  //grabbing indexes of clicked and empty buttons
+
+        if (isValidSwap(clickedIndex, emptyIndex)) {
+            swap(buttonClicked, emptyButton);
+        }
 
     }
 
+    private boolean isValidSwap(int clickedIndex, int emptyIndex) {
+        int difference = Math.abs(clickedIndex - emptyIndex);
+        if (difference == 1 || difference == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     private void swap(JButton buttonOne, JButton buttonTwo) {
-        Container temp = buttonOne.getParent();
+        Component temp = buttonOne.getParent();
         Container temp2 = buttonTwo.getParent();
-        temp.setComponentZOrder(temp, buttonOne.);
-
-    }
-
-    private void swapValues(int index1, int index2) {
-        int temp = grid[index1];
-        grid[index1] = grid[index2];
-        grid[index2] = temp;
+        window.setComponentZOrder(buttonOne, buttons.indexOf(temp2)); // This was tough to figure out
+        window.setComponentZOrder(buttonTwo, buttons.indexOf(temp));
+        window.repaint();
 
     }
 }
